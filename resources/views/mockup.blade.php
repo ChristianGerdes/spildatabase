187b7e165a71a82eb734d5da49d9ac8cfd3f047e
Christian Gerdes <christian@tilsalg.dk>
2019-09-23 13:55:11 +0200

    First commit

---
+++ resources/views/mockup.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Mockup</title>
+</head>
+<body class="bg-gray-100 text-gray-800">
+    <div>
+        <header class="bg-white border-b py-4 mb-12">
+            <div class="container mx-auto flex">
+                <div class="w-1/4"></div>
+
+                <div class="flex-1">
+                    <label class="relative block">
+                        <div class="inset-y-0 absolute left-0 flex items-center justify-center px-4">
+                            <svg class="fill-current text-gray-600 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
+                            </svg>
+                        </div>
+                        <input placeholder="Søg efter spil" class="bg-gray-200 text-gray-600 pl-12 rounded py-3 pr-4 focus:outline-none w-full">
+                    </label>
+                </div>
+
+                <div class="w-1/4 flex justify-end">
+                    <div class="w-12 h-12 rounded-full bg-gray-200"></div>
+                </div>
+            </div>
+        </header>
+
+        <div class="container mx-auto">
+            <div class="border-b pb-6 mb-6">
+                <h1 class="text-4xl">SIEGE</h1>
+                <div>17 resultater</div>
+            </div>
+
+            <div class="flex flex-wrap">
+                <div class="w-full md:w-1/4">
+                    <div class="mb-8">
+                        <div class="uppercase block font-semibold pb-3">Genre</div>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>Gyser</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>Action</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>Brætspil</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>RPG</div>
+
+                            <input type="checkbox">
+                        </label>
+                    </div>
+
+                    <div class="mb-8">
+                        <div class="uppercase block font-semibold pb-3">Årstal</div>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>2015</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>2016</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>2017</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>2018</div>
+
+                            <input type="checkbox">
+                        </label>
+                    </div>
+
+                    <div class="mb-8">
+                        <div class="uppercase block font-semibold pb-3">Udviklere</div>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>Steam</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>Ubisoft</div>
+
+                            <input type="checkbox">
+                        </label>
+
+                        <label class="block flex items-center justify-between border-t py-2 select-none">
+                            <div>EA</div>
+
+                            <input type="checkbox">
+                        </label>
+                    </div>
+                </div>
+
+                <div class="w-full px-6 md:w-3/4 md:pl-6 md:pr-0">
+                    <div class="flex flex-wrap -mx-2">
+                        <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
+                            <a href="#" class="block bg-white rounded-lg overflow-hidden shadow mx-2 mb-4 hover:shadow-md hover:text-gray-600">
+                                <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0001-CUSA01800_00-RB6SIEGESPULTI04/1567553387000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000">
+                                <div class="text-center uppercase py-3 text-xs">rainbow six siege</div>
+                            </a>
+                        </div>
+
+                        <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
+                            <a href="#" class="block bg-white rounded-lg overflow-hidden shadow mx-2 mb-4 hover:shadow-md hover:text-gray-600">
+                                <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0001-CUSA01800_00-RB6SIEGESPULTI04/1567553387000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000">
+                                <div class="text-center uppercase py-3 text-xs">rainbow six siege</div>
+                            </a>
+                        </div>
+
+                        <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
+                            <a href="#" class="block bg-white rounded-lg overflow-hidden shadow mx-2 mb-4 hover:shadow-md hover:text-gray-600">
+                                <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0001-CUSA01800_00-RB6SIEGESPULTI04/1567553387000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000">
+                                <div class="text-center uppercase py-3 text-xs">rainbow six siege</div>
+                            </a>
+                        </div>
+
+                        <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
+                            <a href="#" class="block bg-white rounded-lg overflow-hidden shadow mx-2 mb-4 hover:shadow-md hover:text-gray-600">
+                                <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0001-CUSA01800_00-RB6SIEGESPULTI04/1567553387000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000">
+                                <div class="text-center uppercase py-3 text-xs">rainbow six siege</div>
+                            </a>
+                        </div>
+
+                        <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
+                            <a href="#" class="block bg-white rounded-lg overflow-hidden shadow mx-2 mb-4 hover:shadow-md hover:text-gray-600">
+                                <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0001-CUSA01800_00-RB6SIEGESPULTI04/1567553387000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000">
+                                <div class="text-center uppercase py-3 text-xs">rainbow six siege</div>
+                            </a>
+                        </div>
+
+                        <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
+                            <a href="#" class="block bg-white rounded-lg overflow-hidden shadow mx-2 mb-4 hover:shadow-md hover:text-gray-600">
+                                <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0001-CUSA01800_00-RB6SIEGESPULTI04/1567553387000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000">
+                                <div class="text-center uppercase py-3 text-xs">rainbow six siege</div>
+                            </a>
+                        </div>
+
+                        <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
+                            <a href="#" class="block bg-white rounded-lg overflow-hidden shadow mx-2 mb-4 hover:shadow-md hover:text-gray-600">
+                                <img src="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP0001-CUSA01800_00-RB6SIEGESPULTI04/1567553387000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000">
+                                <div class="text-center uppercase py-3 text-xs">rainbow six siege</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::view('/mockup', 'mockup');

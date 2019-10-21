ec6ed8fce60dd108ea4bb7353d7fc24c0c8471b7
Christian Gerdes <christian@tilsalg.dk>
2019-10-21 11:41:38 +0200

    WIP

--- app/Http/Controllers/NewestGamesController.php
+++ app/Http/Controllers/NewestGamesController.php
@@ -8,7 +8,7 @@ class NewestGamesController extends Controller
 {
     public function index()
     {
-        $games = Game::published()->orderBy('published_at')->take(25)->with('publishers', 'credits')->get();
+        $games = Game::published()->orderBy('published_at', 'desc')->take(25)->with('publishers', 'credits')->get();
 
         return view('games.list', compact('games'));
     }
